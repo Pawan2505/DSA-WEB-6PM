dac249296e470d27cba9e17cf48e0fd6f627c7e3
pawan2505 <pawanaktu@gmail.com>
2025-01-16 19:26:50 +0530

    Linked List : Insert at Tail

--- code.cpp
+++ code.cpp
@@ -3088,3 +3088,70 @@
 
 // Size Management: No need for resizing.
 
+
+
+// Linked list : 1> insert at tail
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public :
+    int data;
+    Node *next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtTail(Node* &head, int data){
+
+    Node *newNode = new Node(data);
+
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    } 
+
+    temp->next = newNode;
+
+}
+
+
+void display(Node* &head){
+
+    if(head == NULL){
+        cout<<"Linked List Empty!"<<endl;
+    }
+
+    Node *temp = head;
+
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    } 
+
+    cout<<"NULL"<<endl;
+}
+
+
+int main(){
+    Node *head = NULL;
+
+    insertAtTail(head,10);
+    insertAtTail(head,20);
+    insertAtTail(head,30);
+    insertAtTail(head,40);
+    insertAtTail(head,50);
+    insertAtTail(head,60);
+
+    display(head);
+}
